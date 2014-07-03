c21aa6a28ee863d6f742ffccd114cfe0b9957dde
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-03 18:01:25 +1200

    Added assoc tests to Game::Unit spec

--- spec/models/game/unit_spec.rb
+++ spec/models/game/unit_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Game::Unit, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :city }
+  it { should belong_to :player }
 end
