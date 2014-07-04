3efe0b55d7461cfbed888fdcc648c6d9a1b1664a
Taylor Lodge <ubermouse894@gmail.com>
2014-07-04 17:36:08 +1200

    Factories create models immediately Factories no longer generate random model ids

--- lib/game/factories.rb
+++ lib/game/factories.rb
@@ -5,12 +5,11 @@ module Game
     class Unit
       def self.make(args = {})
         attributes = {
-            id: rand(50000),
             unit_type: 'infantry',
             attack: 1,
             defense: 1
         }.merge(args)
-        Game::Unit.new(attributes)
+        Game::Unit.create(attributes)
       end
     end
 
@@ -18,18 +17,18 @@ module Game
     class City
       def self.make(args = {}, num_units = 2)
         attributes = {
-        id: rand(50000),
         name: Faker::Address.city,
         latitude: Faker::Address.latitude,
         longitude: Faker::Address.longitude,
         population: rand(2_500_000)
         }.merge(args)
 
-        city = Game::City.new(attributes)
+        city = Game::City.create(attributes)
         num_units.times {
           city.units << Unit.make(city_id: city.id)
         }
 
+        city.save
         city
       end
 
@@ -38,16 +37,16 @@ module Game
     class Player
       def self.make(args = {}, num_cities = 2, num_units_per_city = 2)
         attributes = {
-            id: rand(50000),
             match_id: 1,
             name: Faker::Internet::user_name
         }.merge(args)
 
-        player = Game::Player.new(attributes)
+        player = Game::Player.create(attributes)
         num_cities.times {
           player.cities << City.make({player_id: player.id}, num_units_per_city)
         }
 
+        player.save
         player
       end
     end
