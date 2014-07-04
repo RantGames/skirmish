f633f7fb865486cd965f1687922972fbe8e34d61
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-04 15:35:00 +1200

    Reimplemented Session controller to allow ajax login with json + test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   get 'game_state/show'
 
-  devise_for :users
+  devise_for :users, :controllers => {sessions: 'sessions'}
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
