a4135e34a6edfddc149ea4ea38f18bee49c7edc7
Richard Wood <rwoodnz@gmail.com>
2014-07-04 23:33:03 +1200

    handling new user request depending on whether board is full

--- app/models/game/match.rb
+++ app/models/game/match.rb
@@ -1,4 +1,26 @@
 class Game::Match < ActiveRecord::Base
   has_many :players
   has_many :turns
+
+  def self.allocate_match
+    self.setup_new_game_state if self.last.full?
+    self.setup_in_latest_match
+  end
+
+  def full?
+    self.cities.all? {|city| city.id != nil}
+  end
+
+  def cities
+    self.players.map(&:cities).flatten
+  end
+
+  def self.setup_new_game_state
+    # Game::GameState.new() - with player id - sort out 2 player start issue
+  end
+
+  def self.setup_in_latest_match
+    # allocate user to randomly selected barbarian city(s)
+  end
+
 end
