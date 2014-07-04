cd904098d3d46ea1f0bae30dd40048ce36b1f925
Richard Wood <rwoodnz@gmail.com>
2014-07-04 12:25:53 +1200

    started on json out

--- spec/models/game/game_state_spec.rb
+++ spec/models/game/game_state_spec.rb
@@ -24,4 +24,12 @@ describe Game::GameState do
       expect(updated_state.players[i].cities[0].units).to eq(expected_units)
     end
   end
+
+  pending it 'can convert game_state to json given id' do
+    game_state_in_json = "{players:[{id:1,name:'ubermouse',cities:[{id:1,name:'Copenhagen',latitude:55.6712674,longitude:12.5608388,units:[{id:1,type:'infantry',attack:1,defense:1},{id:2,type:'infantry',attack:1,defense:1},]}]},{id:2,name:'widdershin',cities:[{id:2,name:'Wellington',latitude:-41.2443701,longitude:174.7618546,units:[{id:3,unit_type:'infantry',attack:1,defense:1},{id:4,unit_type:'infantry',attack:1,defense:1},{id:5,unit_type:'infantry',attack:1,defense:1},]}]}]}"
+
+    expect(GameState.json_by_id(1)).to equal(game_state_in_json)
+
+  end
+
 end
