1696b7e4761f53396aa5b127020c19a30cd000ce
Richard Wood <rwoodnz@gmail.com>
2014-07-05 11:05:00 +1200

    handle case of no existing board

--- app/models/skirmish/game.rb
+++ app/models/skirmish/game.rb
@@ -3,7 +3,7 @@ class Skirmish::Game < ActiveRecord::Base
   has_many :turns
 
   def self.allocate_game
-    if self.last.full?
+    if !self.exists? || self.last.full?
       self.setup_new_game_state
     else
       self.setup_in_latest_match
@@ -19,7 +19,8 @@ class Skirmish::Game < ActiveRecord::Base
   end
 
   def self.setup_new_game_state
-    # Game::GameState.new() - with player id - sort out 2 player start issue
+    # Game::GameState.new() - with player id? - sort out 2 player start issue
+    self.setup_in_latest_match
   end
 
   def self.setup_in_latest_match
