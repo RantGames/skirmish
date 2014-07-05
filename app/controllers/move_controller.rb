b3ac381e35d29ce7b8e0bfe1f928d673811e9dbb
Taylor Lodge <ubermouse894@gmail.com>
2014-07-05 12:43:54 +1200

    MoveController#create accepts a move and validates it

--- app/controllers/move_controller.rb
+++ app/controllers/move_controller.rb
@@ -4,6 +4,18 @@ class MoveController < ApplicationController
   def create
     move = params[:move]
     game_id = params[:game_id]
-    player = current_user
+    player = Player.find_by_user_id(current_user.id)
+    game_state = GameState.from_match game_id
+    move = Move.new(game_id: game_id,
+                    player_id: player.id,
+                    action: move['action'],
+                    origin_id: move['origin_id'],
+                    target_id: move['target_id'])
+
+    if move.validate(game_state) && move.save
+      render json: {message: 'Move created'}
+    else
+      render json: {message: 'There was a problem creating your move'}
+    end
   end
 end
