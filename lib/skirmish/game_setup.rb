e5cb67e112861571db22652e5d675caac39d4c05
Richard Wood <rwoodnz@gmail.com>
2014-07-05 17:33:24 +1200

    refactoring with barbarian flag

--- lib/skirmish/game_setup.rb
+++ lib/skirmish/game_setup.rb
@@ -3,11 +3,12 @@ module Skirmish::GameSetup
 BARBARIAN_NAME = 'Neutral'
 CITY_MIN_POPULATION = 150000
 ANGLE_LAT_LONG = 10
+DEFAULT_UNIT_QUANTITY = 1
 
   def self.setup_new_game_state
     game = Skirmish::Game.new
-    setup_barbarian(game)
-    setup_cities(game)
+    barbarian = add_new_barbarian(game)
+    setup_cities(barbarian)
     game
   end
 
@@ -17,21 +18,25 @@ ANGLE_LAT_LONG = 10
 
   private
 
-  def self.setup_barbarian(game)
-    barbarian = Skirmish::Player.find_by_id(1) || Skirmish::Player.new(id:1, name:BARBARIAN_NAME)
+  def self.add_new_barbarian(game)
+    barbarian = Skirmish::Player.create({
+      name:BARBARIAN_NAME,
+      barbarian:true })
     game.players << barbarian
-    game
+    barbarian
   end
 
-  def self.setup_cities(game)
-    city_list = Skirmish::CityList.random_cities(CITY_MIN_POPULATION,ANGLE_LAT_LONG)
+  def self.setup_cities(barbarian)
+    city_list = Skirmish::CityList.random_cities(
+      CITY_MIN_POPULATION,
+      ANGLE_LAT_LONG)
     city_list.each do |city|
-      game.cities << Skirmish::Factories::City.make({id:1,
+      barbarian.cities << Skirmish::Factories::City.make({
                               name:city.name,
                               latitude: city.latitude,
                               longitude: city.longitude,
                               population: city.population
-                              },1)
+                              },DEFAULT_UNIT_QUANTITY)
     end
 
   end
