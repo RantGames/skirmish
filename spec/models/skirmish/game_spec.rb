1649f880afaa776eba9f041ae2a32edd65a52a4f
Richard Wood <rwoodnz@gmail.com>
2014-07-05 12:32:06 +1200

    fixed doubled up test and double negative method

--- spec/models/skirmish/game_spec.rb
+++ spec/models/skirmish/game_spec.rb
@@ -10,7 +10,6 @@ describe 'allocate match' do
   before do
     @game = Skirmish::Game.new
     @game.players = [Skirmish::Factories::Player.make]
-
     allow(Skirmish::Game).to receive(:last).and_return(@game)
   end
 
@@ -18,11 +17,11 @@ describe 'allocate match' do
 
     it 'game has cities with no player id' do
       @game.players.map(&:cities)[0][0].id = nil
-      expect(@game.not_full?).to eq(true)
+      expect(@game.full?).to eq(false)
     end
 
     it 'if board not full, send last board' do
-      allow(@game).to receive(:not_full?).and_return(true)
+      allow(@game).to receive(:full?).and_return(false)
       Skirmish::Game.should_receive(:setup_in_latest_match)
       Skirmish::Game.allocate_game
     end
@@ -31,17 +30,26 @@ describe 'allocate match' do
 
   context 'board full' do
 
-    it 'game has no cities with no player id' do
-      expect(@game.not_full?).to eq(true)
+    it 'all cities have valid player ids' do
+      expect(@game.full?).to eq(true)
     end
 
     it 'if board full, setup_new_match' do
-      allow(@game).to receive(:not_full?).and_return(false)
+      allow(@game).to receive(:full?).and_return(true)
       Skirmish::Game.should_receive(:setup_new_game_state)
       Skirmish::Game.allocate_game
     end
 
   end
 
+  context 'no board' do
+
+    it 'when no match, setup_new_match' do
+      allow(@match).to receive(:exists?).and_return(false)
+      expect(Skirmish::Game).to receive(:setup_new_game_state)
+      Skirmish::Game.allocate_game
+    end
+
+  end
 
 end
