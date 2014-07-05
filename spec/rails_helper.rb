ed06145583a3e3c88d8cc6da460741905f38b067
Taylor Lodge <ubermouse894@gmail.com>
2014-07-05 16:21:36 +1200

    Put newly created moves in current game turn. MoveController tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -23,6 +23,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
+  config.include DeviseHelpers, :type => :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
