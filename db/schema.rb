8e9604b26af04970d499bcfc9eb8c84cf828e426
Nick Johnstone <ncwjohnstone@gmail.com>
2014-07-06 14:44:53 +1200

    updated schema after removing migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140705114321) do
+ActiveRecord::Schema.define(version: 20140705104632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,11 +43,11 @@ ActiveRecord::Schema.define(version: 20140705114321) do
 
   create_table "players", force: true do |t|
     t.integer  "game_id"
+    t.integer  "user_id"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "barbarian",  default: false
-    t.integer  "user_id"
   end
 
   create_table "turns", force: true do |t|
