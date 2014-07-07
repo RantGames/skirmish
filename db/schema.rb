2888a31148aef416cd14930849669f903eb33691
Taylor Lodge <ubermouse894@gmail.com>
2014-07-07 21:58:06 +1200

    Fixed unit attacking

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140706034223) do
+ActiveRecord::Schema.define(version: 20140707084951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20140706034223) do
     t.integer  "game_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "completed"
   end
 
   create_table "units", force: true do |t|
