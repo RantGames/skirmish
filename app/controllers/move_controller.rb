55f140b9a3fd900053747e7fc9b7a187d9615fd2
Samson Ootoovak <sam@enspiral.com>
2014-07-08 11:37:58 +1200

    Move 'Move' logic from the controller into a new factory model.

--- app/controllers/move_controller.rb
+++ app/controllers/move_controller.rb
@@ -2,27 +2,17 @@ class MoveController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    move_json = params[:move]
-    game_id = params[:game_id]
-    player = Skirmish::Player.where(user_id: current_user.id, game_id: game_id).first
-    game_state = Skirmish::GameState.from_game game_id
-    move = Skirmish::Move.new(
-                    player_id: player.id,
-                    action: move_json['action'],
-                    target_id: move_json['target_id'])
-    move_json['origin_ids'].each do |id|
-      move.move_origins.new(origin_id: id)
-    end
+    args = {
+      move_json: params[:move],
+      game_id: params[:game_id],
+      user: current_user,
+    }
+    move = Skirmish::Move::Factory.build(args)
 
-    error_message = move.validate(game_state)
-
-    if error_message.empty? && move.save
-      Skirmish::Turn.add_move(move, game_state.game)
-      render json: {message: 'Move created'}
-    elsif not error_message.empty?
-      render json: {message: "Error occurred processing move: #{error_message}"}, status: 422
+    if move.save
+      render json: { message: "Move created" }
     else
-      render json: {message: 'There was a problem creating your move'}
+      render json: { message: move.error_message }, status: 422
     end
   end
 end
