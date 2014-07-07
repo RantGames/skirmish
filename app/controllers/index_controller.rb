22190027087b0528bcbfd560fd3e8382072f9cf7
Taylor Lodge <ubermouse894@gmail.com>
2014-07-07 18:55:52 +1200

    Fixed index_controller#current_user and make turns only process if there are > 1 players

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,7 @@
 class IndexController < ApplicationController
   before_action :authenticate_user!
 
-  def current_user
+  def current_user_id
     render json: {user_id: current_user.id}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'game_state/show'
   get 'game_state/new'
-  get '/current_user', to: 'index#current_user'
+  get '/current_user', to: 'index#current_user_id'
   post 'move/create'
 
   devise_for :users, :controllers => {sessions: 'sessions'}
