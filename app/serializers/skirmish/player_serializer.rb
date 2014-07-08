c29e247df81887a4c0f9ecc2de0f42c3e7bcff40
Taylor Lodge <ubermouse894@gmail.com>
2014-07-08 15:16:47 +1200

    Include gravatar hash in generated Player json

--- app/serializers/skirmish/player_serializer.rb
+++ app/serializers/skirmish/player_serializer.rb
@@ -1,4 +1,12 @@
+require 'digest/md5'
+
 class Skirmish::PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :user_id
+  attributes :id, :name, :user_id, :gravatar_hash
   has_many :cities
+
+  def gravatar_hash
+    formatted_email = object.user.present? ? object.user.email.strip.downcase : 'barbarian'
+    email_hash = Digest::MD5.hexdigest(formatted_email)
+    email_hash
+  end
 end
