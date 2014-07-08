1c87918b89b100ef51027f961e0313579ddd2e81
Taylor Lodge <ubermouse894@gmail.com>
2014-07-08 15:01:15 +1200

    Rename property in returned JSON data. Closes #44

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,6 +2,6 @@ class IndexController < ApplicationController
   before_action :authenticate_user!
 
   def current_player_id
-    render json: {user_id: current_user.current_player.id}
+    render json: {player_id: current_user.current_player.id}
   end
 end
